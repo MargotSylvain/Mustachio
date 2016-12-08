5ef8808ca83607c576fd2b862e0ed7abca7b18e1
matthew goodwin <matthewjgoodwin@gmail.com>
2016-12-08 12:22:25 +0100

    datatypes imdb and tmd converted to strings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207203503) do
+ActiveRecord::Schema.define(version: 20161208104551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(version: 20161207203503) do
   end
 
   create_table "movies", force: :cascade do |t|
-    t.integer  "imdb_id"
-    t.integer  "mdb_id"
     t.string   "title"
     t.string   "synopsis"
     t.string   "photo"
@@ -37,6 +35,8 @@ ActiveRecord::Schema.define(version: 20161207203503) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "year"
+    t.string   "imdb_id"
+    t.string   "mdb_id"
   end
 
   create_table "tag_collections", force: :cascade do |t|
