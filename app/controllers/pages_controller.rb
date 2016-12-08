52e7dd94565c7882575c7ef96602f127f6cef8a9
matthew goodwin <matthewjgoodwin@gmail.com>
2016-12-08 14:54:26 +0100

    working api call, with ruby/json parser. next need to call the second api for backdrops

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,13 @@ class PagesController < ApplicationController
 
   end
   def search
-      @movie  = Movie.where(title: params[:movie][:title], year: params[:movie][:year].to_i)
-      if movie.any?
+      @title = params[:movie][:title]
+      @year = params[:movie][:year]
+      @movie  = Movie.where(title: @title, year: @year.to_i)
+
+      if @movie.any?
         @movie
+        raise
       else
             # Call api
         result = call_api
@@ -23,7 +27,19 @@ class PagesController < ApplicationController
 
 
   def call_api
+
+    require 'json'
+    require 'open-uri'
+
+    url = "https://www.omdbapi.com/?t=#{@title}&y=#{@year}&plot=short&r=json"
+
+    movie = open(url).read
+
+    movie_obj = JSON.parse(movie)
     raise
+    @imdb_id = movie_obj["imdbID"] #=> returns id as a string
+    @imdb_title = movie_obj["Title"] #=> returns id as a string
+    @imdb_genre = movie_obj["Genre"]
   end
 
 end
