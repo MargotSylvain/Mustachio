5167ea75ae05fcaa6243ed98deffbd5ff3ac18f3
Margot Sylvain <margot.sylvain@gmail.com>
2016-12-08 15:52:49 +0100

    fixing seed file

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,7 +3,7 @@ class Collection < ApplicationRecord
   belongs_to :user
 
   has_many :tags, through: :tag_collections
-  has_many :tag_collections
+  has_many :tag_collections, dependent: :destroy
 
   validates :movie_id, :user_id, presence: true
   validates :movie_id, uniqueness: {scope: :user_id}
