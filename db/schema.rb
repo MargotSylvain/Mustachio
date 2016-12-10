c38bb93bd6f8abe42b9eb944b72bb395ba1b694a
matthew goodwin <matthewjgoodwin@gmail.com>
2016-12-10 01:23:54 +0100

    media model added. id=1, movie; id=2, series; MediaType.all

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209173812) do
+ActiveRecord::Schema.define(version: 20161210001141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20161209173812) do
     t.integer  "user_id"
     t.index ["friend_id"], name: "index_friendships_on_friend_id", using: :btree
     t.index ["user_id"], name: "index_friendships_on_user_id", using: :btree
+  end
+
+  create_table "media_types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "movies", force: :cascade do |t|
